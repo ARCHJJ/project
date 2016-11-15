d81c8ed8cca6e36195a3edd00307c2a5c3172f25
jogle2009 <jogle2009@naver.com>
2016-11-16 03:58:28 +0900

    파일 오픈(기타, 드럼) + 한글패치

--- src/FileOpen.java
+++ src/FileOpen.java
@@ -9,7 +9,7 @@ class FileOpen {
 	FileOpen()
 	{
 		PianoSoundNames = new String[]{"쉼표", "도", "도#", "레", "레#", "미", "파", "파#", "솔", "솔#", "라", "라#", "시"};
-		DrumSoundNames = new String[]{"쉼표", "K", "H", "S", "C"};
+		DrumSoundNames = new String[]{"쉼표", "베이스", "하이헷", "스네어", "라이드", "크래시", "스몰 탐", "하이 탐"};
 		GuitarSoundNames = new String[]{"쉼표", "6", "5", "4", "3", "2", "1"};
 		BaseSoundNames = new String[]{"쉼표", "4", "3", "2", "1"};
 		
@@ -27,8 +27,38 @@ class FileOpen {
 		};
 		for(int i=0; i<3; i++)
 			for(int j=0; j<12; j++)
-				PianoFiles[i][j] = new File(PianoFolder[i]+PianoFnames[i][j]);		
+				PianoFiles[i][j] = new File(PianoFolder[i]+PianoFnames[i][j]);	
 		
+		DrumFolder = new String[] {"C:\\workspace\\project\\drum\\"};
+		DrumFiles = new File[1][7];
+		DrumFnames = new String[][]{{"베이스.wav", "하이헷.wav", "스네어.wav", "라이드.wav", "크래시.wav", "스몰탐.wav", "하이탐.wav"}};
+		for(int i=0; i<7; i++)
+			DrumFiles[0][i] = new File(DrumFolder[0]+DrumFnames[0][i]);	
+		
+		GuitarFolder = new String[] {"C:\\workspace\\project\\guitar\\"};
+		GuitarFiles = new File[6][25];
+		GuitarFnames = new String[][]{{"0.wav", "1.wav", "2.wav", "3.wav", "4.wav", "5.wav", "6.wav", "7.wav", "8.wav", "9.wav", "10.wav", "11.wav", "12.wav"
+										, "13.wav", "14.wav", "15.wav", "16.wav", "17.wav", "18.wav", "19.wav", "20.wav", "21.wav", "22.wav", "23.wav", "24.wav"}         
+		
+									, {"5.wav", "6.wav", "7.wav", "8.wav", "9.wav", "10.wav", "11.wav", "12.wav", "13.wav", "14.wav", "15.wav", "16.wav", "17.wav"
+										, "18.wav", "19.wav", "20.wav", "21.wav", "22.wav", "23.wav", "24.wav", "25.wav", "26.wav", "27.wav", "28.wav", "29.wav"}
+									
+									, {"10.wav", "11.wav", "12.wav", "13.wav", "14.wav", "15.wav", "16.wav", "17.wav", "18.wav", "19.wav", "20.wav", "21.wav", "22.wav"
+										, "23.wav", "24.wav", "25.wav", "26.wav", "27.wav", "28.wav", "29.wav", "30.wav", "31.wav", "32.wav", "33.wav", "34.wav"}
+									
+									, {"15.wav", "16.wav", "17.wav", "18.wav", "19.wav", "20.wav", "21.wav", "22.wav", "23.wav", "24.wav", "25.wav", "26.wav", "27.wav"
+										, "28.wav", "29.wav", "30.wav", "31.wav", "32.wav", "33.wav", "34.wav", "35.wav", "36.wav", "37.wav", "38.wav", "39.wav"}
+									
+									, {"19.wav", "20.wav", "21.wav", "22.wav", "23.wav", "24.wav", "25.wav", "26.wav", "27.wav", "28.wav", "29.wav", "30.wav", "31.wav"
+										, "32.wav", "33.wav", "34.wav", "35.wav", "36.wav", "37.wav", "38.wav", "39.wav", "40.wav", "41.wav", "42.wav", "43.wav"}
+									
+									, {"24.wav", "25.wav", "26.wav", "27.wav", "28.wav", "29.wav", "30.wav", "31.wav", "32.wav", "33.wav", "34.wav", "35.wav", "36.wav"
+										, "37.wav", "38.wav", "39.wav", "40.wav", "41.wav", "42.wav", "43.wav", "44.wav", "45.wav", "46.wav", "47.wav", "48.wav"}
+									};
+										
+		for(int i=0; i<6; i++)
+			for(int j=0; j<25; j++)
+				GuitarFiles[i][j] = new File(GuitarFolder[0]+GuitarFnames[i][j]);
 	}
 	
 	
