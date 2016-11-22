d402b9a73a3cec3adc08456b669a48d88d5ebd2b
jogle2009 <jogle2009@naver.com>
2016-11-22 21:34:56 +0900

    코드입력

---
+++ src/input_GuitarCode.java
@@ -0,0 +1,35 @@
+import java.util.StringTokenizer;
+
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+
+public class input_GuitarCode {
+	private String[][] Code;
+	
+	public input_GuitarCode(String[][] Code_File)
+	{
+		Code = Code_File;
+	}
+	
+	public void addCode(JTable Desk, int x, int y)
+	{
+		String Note;
+		JComboBox temp;
+		StringTokenizer st = new StringTokenizer(Code[x][y] ,",");
+		
+		for(int i=1; i<7; i++)
+		{
+			Note = st.nextToken();
+			
+			temp = (JComboBox)Desk.getModel().getValueAt(i, Desk.getModel().getColumnCount()-1);
+			
+			if(Note.equals("x"))
+				temp.setSelectedIndex(0);
+			else
+				temp.setSelectedIndex(Integer.parseInt(Note)+1);
+			
+			Desk.setValueAt(temp, i, Desk.getModel().getColumnCount()-1);
+			
+		}
+	}
+}
