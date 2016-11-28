76a08c041703ad15d63c313f68f8c4f38c9d2dc8
heeeuncho <jhe1206@naver.com>
2016-11-28 21:59:35 +0900

    SettingToKind 수정

--- src/SettingToKind.java
+++ src/SettingToKind.java
@@ -18,13 +18,7 @@ class SettingToKind extends Setting{
 		field[0][0] = fileNames[0];
 		for(int i=0; i<kinds; i++)
 		{
-			String str;
-			if(fileNames[i].indexOf(".") != -1)
-				str = fileNames[i].substring(0, fileNames[i].indexOf("."));
-			else
-				str = fileNames[i];
-			
-			field[i][0] = str;
+			field[i][0] = fileNames[i];   
 		}
 		tablemodel.setDataVector(field, header);
 	}
